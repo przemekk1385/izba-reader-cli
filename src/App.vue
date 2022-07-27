0b68386bab1d39656bf1c5e2376cb13b2371eaa3
Przemysław Kaliś <przemek.kalis@gmail.com>
2022-07-28 01:02:34 +0200

    add notifications, styling etc.

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,22 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view />
+  <n-config-provider local="en_US" :theme="darkTheme">
+    <n-notification-provider>
+      <n-layout content-style="padding: 24px">
+        <router-view />
+      </n-layout>
+      <n-back-top />
+    </n-notification-provider>
+    <n-global-style />
+  </n-config-provider>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+<script setup lang="ts">
+import {
+  darkTheme,
+  NBackTop,
+  NConfigProvider,
+  NGlobalStyle,
+  NLayout,
+  NNotificationProvider,
+} from "naive-ui";
+</script>
