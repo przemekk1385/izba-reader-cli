befb246f54b0be2c584c3ed883e9e478b0dbc7a0
Przemysław Kaliś <przemek.kalis@gmail.com>
2022-07-28 01:03:24 +0200

    add components

---
+++ src/components/TheArticles.vue
@@ -0,0 +1,48 @@
+<template>
+  <n-grid cols="1" y-gap="24">
+    <n-gi v-for="{ title, description, url, uuid } in modelValue" :key="uuid">
+      <n-card>
+        <template #header>
+          <n-h2 prefix="bar">
+            <n-text type="primary">{{ title }}</n-text>
+          </n-h2>
+        </template>
+        <n-text>{{ description }}</n-text>
+        <template #footer>
+          <n-icon :component="Link" /> <n-a :href="url">{{ urlHost(url) }}</n-a>
+        </template>
+        <template #action>
+          <n-button circle secondary strong type="warning">
+            <template #icon>
+              <n-icon :component="TrashCan" @click="deleteItem(uuid)" />
+            </template>
+          </n-button>
+        </template>
+      </n-card>
+    </n-gi>
+  </n-grid>
+</template>
+
+<script setup lang="ts">
+import { defineEmits, defineProps } from "vue";
+import { NA, NButton, NCard, NGi, NGrid, NH2, NIcon, NText } from "naive-ui";
+import { Link, TrashCan } from "@vicons/carbon";
+
+import type { Feed, News } from "@/types";
+
+const emits = defineEmits(["update:modelValue"]);
+const props = defineProps<{
+  modelValue: (Feed | News)[];
+}>();
+
+const deleteItem = (lookupValue: string) => {
+  const { modelValue: items } = props;
+  const i: number = items.findIndex(({ uuid }) => uuid === lookupValue);
+
+  items.splice(i, 1);
+
+  emits("update:modelValue", items);
+};
+
+const urlHost = (url: string): string => new URL(url).host;
+</script>
---
+++ src/components/ThePreloader.vue
@@ -0,0 +1,17 @@
+<template>
+  <n-grid :cols="1" :y-gap="24">
+    <n-gi v-for="i in randomNumber(2, 5)" :key="i">
+      <n-card>
+        <n-skeleton text :repeat="3" />
+        <n-skeleton text style="width: 75%" />
+      </n-card>
+    </n-gi>
+  </n-grid>
+</template>
+
+<script setup lang="ts">
+import { NCard, NGi, NGrid, NSkeleton } from "naive-ui";
+
+const randomNumber = (min: number, max: number): number =>
+  Math.floor(Math.random() * (max - min)) + min;
+</script>
