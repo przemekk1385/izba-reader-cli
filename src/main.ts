7417d94e255d81031a92d260f3a6a959a7eda807
Przemysław Kaliś <przemek.kalis@gmail.com>
2022-08-13 13:27:30 +0200

    add crypto error fix

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,12 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 
+// Fix TS2339: Property 'randomUUID' does not exist on type 'Crypto'.
+// Based on: https://github.com/denoland/deno/issues/12754#issuecomment-970386235
+declare global {
+  interface Crypto {
+    randomUUID: () => string;
+  }
+}
+
 createApp(App).use(router).mount("#app");
